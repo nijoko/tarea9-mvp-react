86940276071b280ff2ce7bc0b524b3e9df5c688b
nijoko <juan.mamani@usm.cl>
2025-11-07 00:15:00 -0300

    feat: add file download utility
    Implement downloadText function to programmatically trigger text file downloads with custom MIME types and automatic blob cleanup

---
+++ src/utils/download.js
@@ -0,0 +1,9 @@
+export function downloadText(filename, text, mime = "text/plain") {
+  const blob = new Blob([text], { type: mime });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  a.click();
+  setTimeout(() => URL.revokeObjectURL(url), 0);
+}
